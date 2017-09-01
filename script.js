09d5c3232f13ab17c9a12dd8cca02b0642a51297
Travis Batts <Travisbatts6@gmail.com>
2017-09-01 01:24:44 -0400

    lost of styling changes, about to refactor the whole splash page, it needs some better mobile functionality

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ $(document).ready(function(){
 
 //teseting some JQuery with elements on html
 //minimizing the amount of CSS usage
+
 // $("#test").mouseover(function(){
 // $(this).css("text-decoration", "line-through");
 // }),
 // $("#test").mouseleave(function(){
 // $(this).css("text-decoration", "none");
 // });
-
 function blinker() {
-    $('.arrow').fadeOut(500);
-    $('.arrow').fadeIn(500);
+    $('.arrow').fadeOut(500); //fade out arrow svg
+    $('.arrow').fadeIn(500);  //fade in arrow svg
 }
+setInterval(blinker, 2000); //Runs every two seconds
 
-setInterval(blinker, 2000); //Runs every second
+$(window).on('scroll', function(){ //Blur on first picture
+var pixs = $(document).scrollTop()
+pixs = pixs / 100
+$('.banner').css({'-webkit-filter': "blur("+pixs+"px)", "filter":
+"blur("+pixs+"px)" })
+});
+
 
 });
